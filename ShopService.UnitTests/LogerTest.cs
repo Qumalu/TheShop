2414731be54a05fc4c799236eb43ccbf6408ec9b
Radakovic Aleksandar <radakovic.alexandar@hotmail.com>
2020-10-05 01:41:19 +0200

    Refactoring ShopService - testing logger log method

---
+++ ShopService.UnitTests/LogerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace TheShop.UnitTests
+{
+    [TestFixture]
+    class LogerTest
+    {
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]       
+        public void Log_WhenCalledIfMessageParamterIsNullOrEmpty_ThrowArgumentNullException(string message)
+        {
+            var logger = new Logger();
+            Assert.That(() => logger.Log(LogMessageType.Info, message), Throws.ArgumentNullException);            
+        }
+    }
+}
